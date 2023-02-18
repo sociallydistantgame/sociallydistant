e96917daa3ce4dc99ee728e7f0ae8066859b9c7f
Jet the Hawk <jet@win11.vm.the-babylon-garden.local.acidiclight.dev>
2023-02-17 22:41:59 -0500

    Set up a JSON settings registry

---
+++ Assets/Scripts/Audio/TrixelAudioSettingsHandler.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+
+using System;
+using Core.Config;
+using UnityEngine;
+using Utility;
+
+namespace Audio
+{
+	public class TrixelAudioSettingsHandler : MonoBehaviour
+	{
+		private float sfxVolume;
+		private float bgmVolume;
+		private float dialogueVolume;
+		
+		
+		private void Awake()
+		{
+			this.AssertAllFieldsAreSerialized(typeof(TrixelAudioSettingsHandler));
+			Registry.Updated += UpdateAudioSettings;
+		}
+
+		private void UpdateAudioSettings()
+		{
+			Registry.GetValueOrSetDefault("trixel.audio.sfxVolume", 1, out sfxVolume);
+			Registry.GetValueOrSetDefault("trixel.audio.bgmVolume", 1, out bgmVolume);
+			Registry.GetValueOrSetDefault("trixel.audio.dialogueVolume", 1, out dialogueVolume);
+		}
+
+		private void Start()
+		{
+			UpdateAudioSettings();
+		}
+	}
+}
