5199833c0f78e90eab8fdb4bac150cbfd3cbc51e
Jet the Hawk <jet@win11.vm.the-babylon-garden.local.acidiclight.dev>
2023-02-21 21:16:38 -0500

    Add mkdir command

---
+++ Assets/Scripts/UI/ScriptableCommands/MakeDirectoryCommand.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using Architecture;
+using UnityEngine;
+using Utility;
+
+namespace UI.ScriptableCommands
+{
+	[CreateAssetMenu(menuName = "ScriptableObject/Scriptable Commands/Make Directory")]
+	public class MakeDirectoryCommand : ScriptableCommand
+	{
+		/// <inheritdoc />
+		protected override void OnExecute()
+		{
+			if (Arguments.Length == 0)
+			{
+				Console.WriteLine("mkdir: usage: cat <filepath>");
+				return;
+			}
+
+			string fullPath = PathUtility.Combine(CurrentWorkingDirectory, Arguments[0]);
+			
+			if (!FileSystem.DirectoryExists(fullPath))
+				FileSystem.CreateDirectory(fullPath);
+		}
+	}
+}
