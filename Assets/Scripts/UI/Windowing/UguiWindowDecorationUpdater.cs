61d33b3d4458a3871f297ef37da01cb014747a13
Jet the Hawk <jet@win11.vm.the-babylon-garden.local.acidiclight.dev>
2023-02-25 00:54:19 -0500

    Add support for inactive border decorations

---
+++ Assets/Scripts/UI/Windowing/UguiWindowDecorationUpdater.cs
@@ -0,0 +1,52 @@
+﻿#nullable enable
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Utility;
+
+namespace UI.Windowing
+{
+	public class UguiWindowDecorationUpdater : MonoBehaviour
+	{
+		private Image decoratorImage = null!;
+		private UguiWindow window = null!;
+		private bool isActive;
+		
+		[Header("dependencies")]
+		[SerializeField]
+		private WindowFocusService focusService = null!;
+
+		[Header("Configuration")]
+		[SerializeField]
+		private Sprite unskinnedActiveDecoration = null!;
+		
+		[SerializeField]
+		private Sprite unskinnedInactiveDecoration = null!;
+
+		private void Awake()
+		{
+			this.AssertAllFieldsAreSerialized(typeof(UguiWindowDecorationUpdater));
+			
+			this.MustGetComponent(out decoratorImage);
+			this.MustGetComponentInParent(out window);
+		}
+
+		private void Update()
+		{
+			bool wasActive = isActive;
+			bool isActiveNow = focusService.FocusedWindow == window;
+			if (wasActive != isActiveNow)
+			{
+				isActive = isActiveNow;
+				UpdateDecorations();
+			}
+		}
+
+		private void UpdateDecorations()
+		{
+			this.decoratorImage.sprite = isActive
+				? unskinnedActiveDecoration
+				: unskinnedInactiveDecoration;
+		}
+	}
+}
