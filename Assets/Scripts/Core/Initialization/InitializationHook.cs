8669556d6cff62373706d334631212fd6d12bfcf
Jet the Hawk <jet@win11.vm.the-babylon-garden.local.acidiclight.dev>
2023-03-01 17:56:50 -0500

    Add support for initialization hooks

---
+++ Assets/Scripts/Core/Initialization/InitializationHook.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+
+using UnityEngine;
+
+namespace Core.Initialization
+{
+	public abstract class InitializationHook : ScriptableObject
+	{
+		[SerializeField]
+		private int priority;
+
+		public int Priority => priority;
+
+		public abstract void RunHook();
+	}
+}
---
+++ Assets/Scripts/Core/Initialization/PrefabSpawnHook.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+using UnityEngine;
+
+namespace Core.Initialization
+{
+	[CreateAssetMenu(menuName = "ScriptableObject/Initialization Hooks/Prefab Spawn Initialization Hook")]
+	public class PrefabSpawnHook : InitializationHook
+	{
+		[SerializeField]
+		private GameObject prefab = null!;
+
+		/// <inheritdoc />
+		public override void RunHook()
+		{
+			Instantiate(prefab);
+		}
+	}
+}
