e4606dfbf8f0889c309e94b69ac4ce7483757297
Jet the Hawk <jet@win11.vm.the-babylon-garden.local.acidiclight.dev>
2023-03-02 23:48:56 -0500

    Add support for custom game platform implementations

---
+++ Assets/Scripts/GamePlatform/GamePlatformBootstrap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Utility;
+
+namespace GamePlatform
+{
+	public class GamePlatformBootstrap : MonoBehaviour
+	{
+		[Header("Holder")]
+		[SerializeField]
+		private GamePlatformHolder platformHolder = null!;
+
+		private void Awake()
+		{
+			this.AssertAllFieldsAreSerialized(typeof(GamePlatformBootstrap));
+			platformHolder.Value = new OfflineGamePlatform();
+		}
+	}
+}
---
+++ Assets/Scripts/GamePlatform/OfflineGamePlatform.cs
@@ -0,0 +1,11 @@
+﻿namespace GamePlatform
+{
+	public class OfflineGamePlatform : IGamePlatform
+	{
+		/// <inheritdoc />
+		public string QueryPlayerName()
+		{
+			return "Player";
+		}
+	}
+}
