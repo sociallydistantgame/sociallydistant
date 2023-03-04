491deadc628de4ea2a5eb7bf40c2120a31fcc14e
Jet the Hawk <jet@win11.vm.the-babylon-garden.local.acidiclight.dev>
2023-03-03 21:07:37 -0500

    Implement Quit to Desktop tray action

---
+++ Assets/Scripts/UI/Shell/TrayActions/QuitToDesktopAction.cs
@@ -0,0 +1,52 @@
+﻿#nullable enable
+
+using System;
+using UI.Popovers;
+using UI.UiHelpers;
+using UnityEngine;
+using UnityEngine.UI;
+using Utility;
+
+namespace UI.Shell.TrayActions
+{
+	[RequireComponent(typeof(Button))]
+	[RequireComponent(typeof(DialogHelper))]
+	public class QuitToDesktopAction : MonoBehaviour
+	{
+		private Button button = null!;
+		private DialogHelper dialogHelper = null!;
+
+		private void Awake()
+		{
+			this.AssertAllFieldsAreSerialized(typeof(QuitToDesktopAction));
+			this.MustGetComponent(out button);
+			this.MustGetComponent(out dialogHelper);
+		}
+
+		private void Start()
+		{
+			button.onClick.AddListener(OnButtonClick);
+		}
+
+		private void OnButtonClick()
+		{
+			if (dialogHelper.AreAnyDialogsOpen)
+				return;
+
+			dialogHelper.AskQuestion(
+				"Quit Sociallu Distant?",
+				"Do you want to quit Socially Distant and return to your host desktop? Your progress will be saved, but any unsaved changes to files will be lost.",
+				null,
+				OnDialogDismissed
+			);
+		}
+
+		private void OnDialogDismissed(bool yes)
+		{
+			if (!yes)
+				return;
+
+			PlatformHelper.QuitToDesktop();
+		}
+	}
+}
