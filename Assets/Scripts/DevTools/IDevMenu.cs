66d2998bcf1c321c706284fa99e61d424be959d7
Jet the Hawk <jet@win11.vm.the-babylon-garden.local.acidiclight.dev>
2023-03-08 22:47:40 -0500

    Add basic dev menu framework

---
+++ Assets/Scripts/DevTools/IDevMenu.cs
@@ -0,0 +1,9 @@
+﻿namespace DevTools
+{
+	public interface IDevMenu
+	{
+		string Name { get; }
+
+		void OnGUI();
+	}
+}
---
+++ Assets/Scripts/DevTools/NetworldDebug.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+using Core;
+using Core.WorldData;
+using GameplaySystems.Networld;
+using Player;
+
+namespace DevTools
+{
+	public class NetworldDebug : IDevMenu
+	{
+		private NetworkSimulationController simulation;
+		private WorldManager world;
+		private PlayerInstanceHolder player;
+		
+		public string Name => "Networld Debugger";
+
+		public NetworldDebug(WorldManager world, NetworkSimulationController simulation, PlayerInstanceHolder playerInstance)
+		{
+			this.world = world;
+			this.simulation = simulation;
+			this.player = playerInstance;
+		}
+		
+		/// <inheritdoc />
+		public void OnGUI()
+		{
+		}
+	}
+}
