80a83333b9df07dcaa2e30fc12d8dd6f478edfe1
Rosie <alkalinethunder@gmail.com>
2023-05-04 19:29:08 -0400

    Tell git to not ignore Assets/GameModes/Career

---
+++ Assets/GameModes/Career/Scripts/GamePlatform/Steamworks/SteamworksGamePlatform.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+
+using GamePlatform;
+using Steamworks;
+
+namespace GameModes.Career.Scripts.GamePlatform.Steamworks
+{
+	public class SteamworksGamePlatform : IGamePlatform
+	{
+		/// <inheritdoc />
+		public string QueryPlayerName()
+		{
+			return SteamClient.Name;
+		}
+	}
+}
