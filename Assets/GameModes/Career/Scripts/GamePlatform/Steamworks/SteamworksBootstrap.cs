5f652163151b33e5d31089e25b9f421c8f2689df
Rosie <alkalinethunder@gmail.com>
2023-05-04 19:29:08 -0400

    Tell git to not ignore Assets/GameModes/Career

---
+++ Assets/GameModes/Career/Scripts/GamePlatform/Steamworks/SteamworksBootstrap.cs
@@ -0,0 +1,53 @@
+﻿#nullable enable
+using System;
+using GamePlatform;
+using Steamworks;
+using UnityEngine;
+using Utility;
+
+namespace GameModes.Career.Scripts.GamePlatform.Steamworks
+{
+	public class SteamworksBootstrap : MonoBehaviour
+	{
+		private bool isSteamRunning;
+		
+		[Header("Holder")]
+		[SerializeField]
+		private GamePlatformHolder platformHolder = null!;
+
+		[Header("Configuration")]
+		[SerializeField]
+		private uint steamAppId;
+
+		private void Awake()
+		{
+			this.AssertAllFieldsAreSerialized(typeof(SteamworksBootstrap));
+
+			try
+			{
+				SteamClient.Init(steamAppId);
+				isSteamRunning = true;
+			}
+			catch (Exception ex)
+			{
+				Debug.LogWarning("Steamworks initialization failed, see upcoming exception in the log. OfflineGamePlatform from the open-source version of the game will be used, online functionality will not work.");
+				Debug.LogException(ex);
+			}
+
+			if (isSteamRunning)
+			{
+				Debug.Log("Overriding current GamePlatform implementation with Steamworks!");
+				platformHolder.Value = new SteamworksGamePlatform();
+			}
+		}
+
+		private void OnDestroy()
+		{
+			if (isSteamRunning)
+			{
+				SteamClient.Shutdown();
+				platformHolder.Value = new OfflineGamePlatform();
+			}
+		}
+	}
+}
