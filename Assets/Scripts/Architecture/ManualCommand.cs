0ca6dfb0a36f8a07e0c4ea11e183286c156417b3
Rosie <alkalinethunder@gmail.com>
2023-05-13 21:53:09 -0400

    Add man command for opening online manual

---
+++ Assets/Scripts/Architecture/ManualCommand.cs
@@ -0,0 +1,19 @@
+﻿#nullable enable
+using System;
+using UnityEngine;
+
+namespace Architecture
+{
+	[CreateAssetMenu(menuName = "ScriptableObject/Commands/Manual Command")]
+	public class ManualCommand : ScriptableCommand
+	{
+		[SerializeField]
+		private string manualUrl = "https://man.sociallydistantgame.com/";
+		
+		/// <inheritdoc />
+		protected override void OnExecute()
+		{
+			System.Diagnostics.Process.Start(manualUrl);
+		}
+	}
+}
