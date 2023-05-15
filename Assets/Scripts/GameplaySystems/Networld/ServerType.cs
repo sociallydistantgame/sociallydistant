8c2f3ad507db430f9fbcb26dd2590b67b916b04d
Rosie <alkalinethunder@gmail.com>
2023-05-14 22:10:35 -0400

    Add exploit classes and attack effectiveness checking

--- Assets/Scripts/GameplaySystems/Networld/ServerType.cs
+++ Assets/Scripts/GameplaySystems/Networld/ServerType.cs
@@ -1,4 +1,6 @@
-﻿namespace GameplaySystems.Networld
+﻿using System;
+
+namespace GameplaySystems.Networld
 {
 	public enum ServerType
 	{
@@ -12,5 +14,20 @@
 		GameServer,
 		Email,
 		Chat
+	}
+
+	[Flags]
+	public enum ServerTypeFlags
+	{
+		Nothing=0,
+		Shell=1,
+		FileTransfer=2,
+		Web=4,
+		WebSecured=8,
+		Database=16,
+		Desktop=32,
+		GameServer=64,
+		Email=128,
+		Chat=256
 	}
 }
