266f3208a89b272ab5855783abf0289d59921373
Rosie <alkalinethunder@gmail.com>
2023-05-14 20:28:53 -0400

    Add file path completions to shell

--- Assets/Scripts/OS/Devices/IEnvironmentVariableProvider.cs
+++ Assets/Scripts/OS/Devices/IEnvironmentVariableProvider.cs
@@ -1,8 +1,12 @@
 ﻿#nullable enable
+using System.Collections.Generic;
+
 namespace OS.Devices
 {
 	public interface IEnvironmentVariableProvider
 	{
+		IEnumerable<string> Keys { get; }
+
 		string this[string key] { get; set; }
 
 		bool IsSet(string variable);
