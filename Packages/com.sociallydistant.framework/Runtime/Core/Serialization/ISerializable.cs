9cc6b0ba9480947d11854de2e470ee098fe9aee7
Ritchie <alkalinethunder@gmail.com>
2023-09-05 17:38:01 -0400

    Move Core.Serialization namespace to system-runtime

--- Packages/com.sociallydistant.framework/Runtime/Core/Serialization/ISerializable.cs
+++ Packages/com.sociallydistant.framework/Runtime/Core/Serialization/ISerializable.cs
@@ -2,6 +2,14 @@
 
 namespace Core.Serialization
 {
+#nullable enable
+	public interface ISerializable
+	{
+		void Write(IDataWriter writer);
+		void Read(IDataReader reader);
+	}
+
+#nullable restore
 	public interface ISerializable<TRevision> where TRevision : Enum
 	{
 		void Serialize(IRevisionedSerializer<TRevision> serializer);
