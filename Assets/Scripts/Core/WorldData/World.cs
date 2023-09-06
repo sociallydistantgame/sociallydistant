e7268e68bbec8d429d072cf06e1cdf9de275e182
Ritchie <alkalinethunder@gmail.com>
2023-09-05 20:09:30 -0400

    Expose world data tables to the system runtime

--- Assets/Scripts/Core/WorldData/World.cs
+++ Assets/Scripts/Core/WorldData/World.cs
@@ -9,60 +9,88 @@ namespace Core.WorldData
 {
 	public class World : IWorld
 	{
-		public readonly WorldDataObject<GlobalWorldData> GlobalWorldState;
-		public readonly WorldDataObject<WorldPlayerData> PlayerData;
-		public readonly WorldDataTable<WorldComputerData> Computers;
-		public readonly WorldDataTable<WorldInternetServiceProviderData> InternetProviders;
-		public readonly WorldDataTable<WorldLocalNetworkData> LocalAreaNetworks;
-		public readonly WorldDataTable<WorldNetworkConnection> NetworkConnections;
-		public readonly WorldDataTable<WorldPortForwardingRule> PortForwardingRules;
-		public readonly WorldDataTable<WorldCraftedExploitData> CraftedExploits;
-		public readonly WorldDataTable<WorldHackableData> Hackables;
+		private readonly WorldDataObject<GlobalWorldData> globalWorldState;
+		private readonly WorldDataObject<WorldPlayerData> playerData;
+		private readonly WorldDataTable<WorldComputerData> computers;
+		private readonly WorldDataTable<WorldInternetServiceProviderData> internetProviders;
+		private readonly WorldDataTable<WorldLocalNetworkData> localAreaNetworks;
+		private readonly WorldDataTable<WorldNetworkConnection> networkConnections;
+		private readonly WorldDataTable<WorldPortForwardingRule> portForwardingRules;
+		private readonly WorldDataTable<WorldCraftedExploitData> craftedExploits;
+		private readonly WorldDataTable<WorldHackableData> hackables;
 
+		
+        /// <inheritdoc />
+        public IWorldDataObject<GlobalWorldData> GlobalWorldState => globalWorldState;
+		
+        /// <inheritdoc />
+        public IWorldDataObject<WorldPlayerData> PlayerData => playerData;
+		
+        /// <inheritdoc />
+        public IWorldTable<WorldComputerData> Computers => computers;
+		
+        /// <inheritdoc />
+        public IWorldTable<WorldInternetServiceProviderData> InternetProviders => internetProviders;
+		
+        /// <inheritdoc />
+        public IWorldTable<WorldLocalNetworkData> LocalAreaNetworks => localAreaNetworks;
+		
+        /// <inheritdoc />
+        public IWorldTable<WorldNetworkConnection> NetworkConnections => networkConnections;
+		
+        /// <inheritdoc />
+        public IWorldTable<WorldPortForwardingRule> PortForwardingRules => portForwardingRules;
+		
+        /// <inheritdoc />
+        public IWorldTable<WorldCraftedExploitData> CraftedExploits => craftedExploits;
+		
+        /// <inheritdoc />
+        public IWorldTable<WorldHackableData> Hackables => hackables;
+		
 		public World(UniqueIntGenerator instanceIdGenerator, DataEventDispatcher eventDispatcher)
 		{
-			GlobalWorldState = new WorldDataObject<GlobalWorldData>(eventDispatcher);
-			PlayerData = new WorldDataObject<WorldPlayerData>(eventDispatcher);
-			Computers = new WorldDataTable<WorldComputerData>(instanceIdGenerator, eventDispatcher);
-			InternetProviders = new WorldDataTable<WorldInternetServiceProviderData>(instanceIdGenerator, eventDispatcher);
-			LocalAreaNetworks = new WorldDataTable<WorldLocalNetworkData>(instanceIdGenerator, eventDispatcher);
-			NetworkConnections = new WorldDataTable<WorldNetworkConnection>(instanceIdGenerator, eventDispatcher);
-			PortForwardingRules = new WorldDataTable<WorldPortForwardingRule>(instanceIdGenerator, eventDispatcher);
-			CraftedExploits = new WorldDataTable<WorldCraftedExploitData>(instanceIdGenerator, eventDispatcher);
-			Hackables = new WorldDataTable<WorldHackableData>(instanceIdGenerator, eventDispatcher);
+			globalWorldState = new WorldDataObject<GlobalWorldData>(eventDispatcher);
+			playerData = new WorldDataObject<WorldPlayerData>(eventDispatcher);
+			computers = new WorldDataTable<WorldComputerData>(instanceIdGenerator, eventDispatcher);
+			internetProviders = new WorldDataTable<WorldInternetServiceProviderData>(instanceIdGenerator, eventDispatcher);
+			localAreaNetworks = new WorldDataTable<WorldLocalNetworkData>(instanceIdGenerator, eventDispatcher);
+			networkConnections = new WorldDataTable<WorldNetworkConnection>(instanceIdGenerator, eventDispatcher);
+			portForwardingRules = new WorldDataTable<WorldPortForwardingRule>(instanceIdGenerator, eventDispatcher);
+			craftedExploits = new WorldDataTable<WorldCraftedExploitData>(instanceIdGenerator, eventDispatcher);
+			hackables = new WorldDataTable<WorldHackableData>(instanceIdGenerator, eventDispatcher);
 			
 		}
 
 		public void Serialize(IWorldSerializer serializer)
 		{
-			GlobalWorldState.Serialize(serializer);
-			Computers.Serialize(serializer, WorldRevision.AddedComputers);
-			InternetProviders.Serialize(serializer, WorldRevision.AddedInternetServiceProviders);
-			LocalAreaNetworks.Serialize(serializer, WorldRevision.AddedInternetServiceProviders);
-			NetworkConnections.Serialize(serializer, WorldRevision.AddedInternetServiceProviders);
+			globalWorldState.Serialize(serializer);
+			computers.Serialize(serializer, WorldRevision.AddedComputers);
+			internetProviders.Serialize(serializer, WorldRevision.AddedInternetServiceProviders);
+			localAreaNetworks.Serialize(serializer, WorldRevision.AddedInternetServiceProviders);
+			networkConnections.Serialize(serializer, WorldRevision.AddedInternetServiceProviders);
 
 			// serialize after networking stuff, because it relies on them.
-			PlayerData.Serialize(serializer);
+			playerData.Serialize(serializer);
 
-			Hackables.Serialize(serializer, WorldRevision.AddedHackables);
-			PortForwardingRules.Serialize(serializer, WorldRevision.AddedPortForwarding);
+			hackables.Serialize(serializer, WorldRevision.AddedHackables);
+			portForwardingRules.Serialize(serializer, WorldRevision.AddedPortForwarding);
 			
-			CraftedExploits.Serialize(serializer, WorldRevision.AddedExploitCrafting);
+			craftedExploits.Serialize(serializer, WorldRevision.AddedExploitCrafting);
 		}
 
 		public void Wipe()
 		{
 			// You must wipe the world in reverse order of how you would create or serialize it.
 			// This ensures proper handling of deleting objects that depend on other objects.
-			this.CraftedExploits.Clear();
-			this.PortForwardingRules.Clear();
-			this.Hackables.Clear();
-			this.PlayerData.Value = default;
-			this.NetworkConnections.Clear();
-			this.LocalAreaNetworks.Clear();
-			this.InternetProviders.Clear();
-			this.Computers.Clear();
-			this.GlobalWorldState.Value = default;
+			this.craftedExploits.Clear();
+			this.portForwardingRules.Clear();
+			this.hackables.Clear();
+			this.playerData.Value = default;
+			this.networkConnections.Clear();
+			this.localAreaNetworks.Clear();
+			this.internetProviders.Clear();
+			this.computers.Clear();
+			this.globalWorldState.Value = default;
 		}
 	}
 }
