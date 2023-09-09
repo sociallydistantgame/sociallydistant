d9fa151002000e7d801a0faa388b4074b502dd7f
Ritchie <alkalinethunder@gmail.com>
2023-09-09 13:27:50 -0400

    Add settings toggles and input fields

---
+++ Assets/Scripts/UI/Widgets/Settings/SettingsToggleWidget.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+
+using System;
+using AcidicGui.Widgets;
+using UnityEngine;
+
+namespace UI.Widgets.Settings
+{
+	public class SettingsToggleWidget : SettingsWidget
+	{
+		public string Title { get; set; } = string.Empty;
+		public string? Description { get; set; }
+		public bool CurrentValue { get; set; }
+		public Action<bool>? Callback { get; set; }
+		
+		/// <inheritdoc />
+		public override WidgetController BuildSettingsWidget(SystemWidgets assembler, RectTransform destination)
+		{
+			SettingsToggleWidgetController toggle = assembler.GetSettingsToggle(destination);
+
+			toggle.Title = Title;
+			toggle.Description = Description;
+			toggle.CurrentValue = this.CurrentValue;
+			toggle.Callback = Callback;
+			
+			return toggle;
+		}
+	}
+}
