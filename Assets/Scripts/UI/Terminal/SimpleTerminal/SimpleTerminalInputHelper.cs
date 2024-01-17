c1217146dffaf0464ff6beef18ed17b5b43b043e
Ritchie <alkalinethunder@gmail.com>
2024-01-17 16:09:15 -0500

    Fix handling of Home and End keys in terminal

--- Assets/Scripts/UI/Terminal/SimpleTerminal/SimpleTerminalInputHelper.cs
+++ Assets/Scripts/UI/Terminal/SimpleTerminal/SimpleTerminalInputHelper.cs
@@ -16,73 +16,36 @@ namespace UI.Terminal.SimpleTerminal
 
 		public void Delete()
 		{
-			if (term.SuppressInput)
-				return;
-			
 			Write("\x7f");
 		}
 
 		public void UpArrow()
 		{
-			if (term.SuppressInput)
-				return;
-
 			this.Write("\x1b[1A");
 		}
 
 		public void DownArrow()
 		{
-			if (term.SuppressInput)
-				return;
-
 			this.Write("\x1b[1B");
 		}
 
 		public void RightArrow()
 		{
-			if (term.SuppressInput)
-				return;
-
 			this.Write("\x1b[1C");
 		}
 
 		public void LeftArrow()
 		{
-			if (term.SuppressInput)
-				return;
-
 			this.Write("\x1b[1D");
 		}
-
-		public void End()
-		{
-			if (term.SuppressInput)
-				return;
-
-			this.Write("\x1b[4~");
-		}
-
-		public void Home()
-		{
-			if (term.SuppressInput)
-				return;
-
-			this.Write("\x1b[1~");
-		}
-
+		
 		public void Backspace()
 		{
-			if (term.SuppressInput)
-				return;
-
 			this.Write("\b");
 		}
 
 		public void Enter()
 		{
-			if (term.SuppressInput)
-				return;
-
 			// We only write a CR, even on POSIX systems, since this maps to Enter in the line editor. Writing
 			// the LF breaks line editor.
 			this.Write("\r");
@@ -90,9 +53,6 @@ namespace UI.Terminal.SimpleTerminal
 
 		public void Char(char ch)
 		{
-			if (term.SuppressInput)
-				return;
-
 			this.Write(ch.ToString());
 		}
 
@@ -134,12 +94,6 @@ namespace UI.Terminal.SimpleTerminal
 						return;
 					case KeyCode.Delete:
 						this.Delete();
-						return;
-					case KeyCode.Home:
-						this.Home();
-						return;
-					case KeyCode.End:
-						this.End();
 						return;
 				}
 			}
