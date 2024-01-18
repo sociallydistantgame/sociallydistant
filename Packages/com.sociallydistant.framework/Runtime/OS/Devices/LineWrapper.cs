fb30d7fab80ed31b4ded514fb0b8214e50a14d32
Ritchie <alkalinethunder@gmail.com>
2024-01-17 21:10:50 -0500

    Add support for selection in line editor

--- Packages/com.sociallydistant.framework/Runtime/OS/Devices/LineWrapper.cs
+++ Packages/com.sociallydistant.framework/Runtime/OS/Devices/LineWrapper.cs
@@ -10,13 +10,21 @@ namespace OS.Devices
 		private readonly StringBuilder wordBuilder = new StringBuilder();
 		private int caretWord;
 		private int caretChar;
+		private int selStartChar;
+		private int selEndChar;
+		private int selStartWord;
+		private int selEndWord;
 
-		private IEnumerable<string> Break(StringBuilder sourceText, int maxWordLength, int caret)
+		private IEnumerable<string> Break(StringBuilder sourceText, int maxWordLength, int caret, int selectionStart, int selectionEnd)
 		{
 			wordBuilder.Length = 0;
 
 			caretChar = 0;
 			caretWord = 0;
+			selStartChar = 0;
+			selStartWord = 0;
+			selEndChar = 0;
+			selEndWord = 0;
 			
 			var wasDelim = true;
 
@@ -25,6 +33,12 @@ namespace OS.Devices
 				if (i == caret)
 					caretChar = wordBuilder.Length;
 
+				if (i == selectionStart)
+					selStartChar = wordBuilder.Length;
+				
+				if (i == selectionEnd)
+					selEndChar = wordBuilder.Length;
+				
 				if (i == sourceText.Length)
 				{
 					if (wordBuilder.Length > 0)
@@ -50,6 +64,12 @@ namespace OS.Devices
 							if (i < caret)
 								caretWord++;
 
+							if (i < selectionStart)
+								selStartWord++;
+
+							if (i < selectionEnd)
+								selEndWord++;
+							
 							yield return wordBuilder.ToString();
 							wordBuilder.Length = 0;
 						}
@@ -64,6 +84,12 @@ namespace OS.Devices
 						if (i < caret)
 							caretWord++;
 						
+						if (i < selectionStart)
+							selStartWord++;
+
+						if (i < selectionEnd)
+							selEndWord++;
+						
 						yield return wordBuilder.ToString();
 						wordBuilder.Length = 0;
 					}
@@ -73,7 +99,7 @@ namespace OS.Devices
 			}
 		}
 
-		public string Wrap(StringBuilder sourceText, int slack, int startY, int maxWidth, int caret, out int caretX, out int caretY, out int height, out int lastLineWidth)
+		public string Wrap(StringBuilder sourceText, int slack, int startY, int maxWidth, int caret, int selectionStart, int selectionEnd, out int caretX, out int caretY, out int height, out int lastLineWidth, out int wrappedSelectionStart, out int wrappedSelectionEnd)
 		{
 			int x = slack;
 			int y = startY;
@@ -82,11 +108,13 @@ namespace OS.Devices
 			caretX = x;
 			caretY = y;
 			height = 1;
+			wrappedSelectionStart = -1;
+			wrappedSelectionEnd = -1;
 
 			stringBuilder.Length = 0;
 
 			var wordIndex = 0;
-			foreach (string word in Break(sourceText, maxWidth, caret))
+			foreach (string word in Break(sourceText, maxWidth, caret, selectionStart, selectionEnd))
 			{
 				if (x + word.Length > maxWidth)
 				{
@@ -102,6 +130,13 @@ namespace OS.Devices
 					caretX = x + caretChar;
 				}
 
+				if (wordIndex == selStartWord)
+					wrappedSelectionStart = stringBuilder.Length + selStartChar;
+				
+				if (wordIndex == selEndWord)
+					wrappedSelectionEnd = stringBuilder.Length + selEndChar;
+
+				
 				stringBuilder.Append(word);
 				x += word.Length;
 				lastLineWidth = x;
