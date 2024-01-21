eea9840f19dd1970e8094931d079bbaf59ce234a
Ritchie <alkalinethunder@gmail.com>
2024-01-21 00:21:30 -0500

    Fix handling of newlines in shell

---
+++ Assets/Scripts/Core/Scripting/Instructions/BranchEvaluator.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OS.Devices;
+
+namespace Core.Scripting.Instructions
+{
+	public sealed class BranchEvaluator : ShellInstruction
+	{
+		private readonly IReadOnlyCollection<ShellInstruction> cases;
+		private readonly IReadOnlyCollection<ShellInstruction> defaultBody;
+
+		public BranchEvaluator(IReadOnlyCollection<ShellInstruction> cases, IReadOnlyCollection<ShellInstruction> defaultBody)
+		{
+			this.cases = cases;
+			this.defaultBody = defaultBody;
+		}
+		
+		/// <inheritdoc />
+		public override async Task RunAsync(ITextConsole console)
+		{
+			// TODO: do this.
+		}
+	}
+}
---
+++ Assets/Scripts/Core/Scripting/ShellTester.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+namespace Core.Scripting
+{
+	public class ShellTester
+	{
+		private readonly string[] expressions;
+
+		public ShellTester(string[] expressions)
+		{
+			this.expressions = expressions;
+		}
+		
+		public int Test()
+		{
+			return 0;
+		}
+	}
+}
