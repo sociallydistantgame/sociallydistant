c33637ca5eceee1a3b7d1c6d510a599b40babcd2
Ritchie Frodomar <alkalinethunder@gmail.com>
2024-07-12 15:22:12 -0400

    Port the Exit Game button

--- src/SociallyDistant/UI/ExitGameTrayAction.cs
+++ src/SociallyDistant/UI/ExitGameTrayAction.cs
@@ -1,4 +1,6 @@
 using SociallyDistant.Core.Modules;
+using SociallyDistant.Core.Shell;
+using SociallyDistant.Core.Shell.Windowing;
 using SociallyDistant.Core.UI.Shell;
 
 namespace SociallyDistant.UI;
@@ -7,9 +9,38 @@ internal sealed class ExitGameTrayAction : TrayAction
 {
     public ExitGameTrayAction(IGameContext context) : base(context)
     {
+        Icon = MaterialIcons.PowerSettingsNew;
     }
 
-    public override void Invoke()
+    public override async void Invoke()
     {
+        bool willExit = await AskToQuit();
+
+        if (!willExit)
+            return;
+
+        await Context.EndCurrentGame(true);
+
+        // bye bye
+        Context.GameInstance.Exit();
+    }
+    
+    private Task<bool> AskToQuit()
+    {
+        var completionSource = new TaskCompletionSource<bool>();
+        var dialog = this.Context.Shell.CreateMessageDialog("Quit Socially Distant");
+
+        dialog.Message = "Are you sure you want to quit the game? Any unsaved files will be lost.";
+        dialog.MessageType = MessageBoxType.Warning;
+        
+        dialog.Buttons.Add(new MessageBoxButtonData("Yes", MessageDialogResult.Yes));
+        dialog.Buttons.Add(new MessageBoxButtonData("No",  MessageDialogResult.No));
+
+        dialog.DismissCallback = result =>
+        {
+            completionSource.SetResult(result == MessageDialogResult.Yes);
+        };
+
+        return completionSource.Task;
     }
 }
